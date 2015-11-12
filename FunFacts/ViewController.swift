2f3d82ea28b4341276f90b324157d34315b2c822
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-12 07:23:35 -0600

    Start fun facts

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func showFunFact() {
+        funFactLabel.text = "Another interesting fact"
+    }
 }
 
